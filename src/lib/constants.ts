0e1fb317e7e6c8192d2feee1da23da6ea727de7e
Jan Henning <triton539@outlook.com>
2024-03-08 12:20:33 +0100

    move const to constants lib file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const PORT = Number(process.env.PORT) || 3000
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+export const ADMIN_API_KEY = process.env.ADMIN_API_KEY
+export const NOROFF_API_KEY_HEADER = "X-Noroff-API-Key"
+export const ADMIN_PROTECTED_ROUTES = ["/api-key"]
